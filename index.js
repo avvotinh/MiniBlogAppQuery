addb51c0b4f30957530dc16bf2023025b707d713
Le Van Hop <hop.le@axonactive.com>
2021-07-07 18:27:35 +0700

    updated

--- index.js
+++ index.js
@@ -22,10 +22,25 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
 
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    console.log("Query", id, status);
+    const commentsByPostId = posts[postId];
+    const comment = commentsByPostId.comments.find(
+      (comment) => comment.id === id
+    );
+
+    console.log(comment);
+
+    comment.content = content;
+    comment.status = status;
   }
 
   res.send({});
